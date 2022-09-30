55fafc62aeeca103ea6f2bc8790afd1899e0c1e7
Makarova Anastasia <korzhak.an@gmail.com>
2022-09-30 12:30:11 +0300

    Views were modified by using ModelViewSet

--- note_pad/api/urls.py
+++ note_pad/api/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from .views import *
 from rest_framework.urlpatterns import format_suffix_patterns #для отображения данных в формате JSON
+from rest_framework.routers import DefaultRouter
 
+#Для автоматического ветвления при использовании вьюсета
 
-urlpatterns = [
-    path('notes/', NotesList.as_view()),
-    path('notes/<int:pk>/', NotesDetailView.as_view(), name='notes-detail')
+router =DefaultRouter()
+router.register('notes', NotesView, basename='notes')
+urlpatterns = router.urls
 
-]
+# urlpatterns = [
+#     # path('notes/', NotesView.as_view({'get': 'list', 'post': 'create'}), name='notes-list'),
+#     # path('notes/<int:pk>/', NotesView.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='notes-detail'),
+#
+# ]
 
-urlpatterns =format_suffix_patterns(urlpatterns)
+# urlpatterns =format_suffix_patterns(urlpatterns)
--- note_pad/api/views.py
+++ note_pad/api/views.py
@@ -1,10 +1,58 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from notes.models import Note
-from .serializers import *
-from rest_framework.response import Response
+from rest_framework.mixins import ListModelMixin,CreateModelMixin
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+
+#через вьюсеты
+class NotesView(ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+    #для переопределения сериалайзера для усечения отображения полей. Если не нужно переопредделять, то эта функция не нужна
+    def list(self, request, *args, **kwargs):
+        notes = Note.objects.all()
+        context = {'request': request}
+        serializer = ThinNoteSerializer(notes, many=True, context=context)
+        return Response(serializer.data)
+
+
+#через дженерики
+# class NotesList(ListCreateAPIView):
+#     queryset = Note.objects.all()
+#     serializer_class = NoteSerializer
+#
+#     def list(self, request, *args, **kwargs):
+#         notes = Note.objects.all()
+#         context = {'request': request}
+#         serializer = ThinNoteSerializer
+#         return Response (serializer.data)
+#
+#
+# class NotesDetailView(RetrieveUpdateDestroyAPIView):
+#     queryset = Note.objects.all()
+#     serializer_class = NoteSerializer
+#
+#
+#
+# определяем через миксин
+#
+# class NotesList(ListModelMixin, CreateModelMixin, GenericAPIView):
+#     queryset = Note.objects.all()
+#     serializer_class = NoteSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         self.serializer_class = ThinNoteSerializer
+#         return self.list(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 
 #определяем через функцию
@@ -22,16 +70,16 @@ from rest_framework.views import APIView
 #         else:
 #             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#определяем через класс
-class NotesList(APIView):
-    def get(self, request, formate=None):
-        notes = Note.objects.all()
-        context = {'request': request}
-        serializer = ThinNoteSerializer(notes, many=True, context=context)
-        return Response(serializer.data)
-
-    def post(self, request, formate=None):
-        serializer = NoteSerializer(data=request.data)
+# #определяем через класс
+# class NotesList(APIView):
+#     def get(self, request, formate=None):
+#         notes = Note.objects.all()
+#         context = {'request': request}
+#         serializer = ThinNoteSerializer(notes, many=True, context=context)
+#         return Response(serializer.data)
+#
+#     def post(self, request, formate=None):
+#         serializer = NoteSerializer(data=request.data)
 
 
 #определяем через функцию
@@ -54,32 +102,32 @@ class NotesList(APIView):
 #     elif request.method == 'DELETE':
 #         note.delete()
 #         return Response(status=status.HTTP_204_NO_CONTENT)status
-
-#определяем через класс
-class NotesDetailView(APIView):
-    def get_object(self, pk, format=None):
-        try:
-            note = Note.objects.get(pk=pk)
-            return note
-        except Note.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-    def get(self, request, pk, format=None):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(note)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(note, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        note = self.get_object(pk)
-        note.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
+#
+# #определяем через класс
+# class NotesDetailView(APIView):
+#     def get_object(self, pk, format=None):
+#         try:
+#             note = Note.objects.get(pk=pk)
+#             return note
+#         except Note.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#     def get(self, request, pk, format=None):
+#         note = self.get_object(pk)
+#         serializer = NoteSerializer(note)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         note = self.get_object(pk)
+#         serializer = NoteSerializer(note, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         note = self.get_object(pk)
+#         note.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+#
