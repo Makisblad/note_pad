3b06b748e83f56a3a29bc586686d52f74dd76058
Makarova Anastasia <korzhak.an@gmail.com>
2022-10-01 14:16:15 +0300

    JWT authentication was added

--- note_pad/note_pad/settings.py
+++ note_pad/note_pad/settings.py
@@ -127,3 +127,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',# для просмотра записей API
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+                                       )
+}
+
